ef48e71ca6030cf3026013dfbbaaa47d7633f088
Sergey Skorykh <kknd3390@mail.ru>
2023-08-08 00:07:05 +0700

    delete

---
+++ src/store/createStore.js
@@ -0,0 +1,23 @@
+export function createStore(reducer, initialState) {
+  let state = initialState || {}
+  let listeners = []
+  function getState() {
+    return state
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action)
+
+    for (const listener of listeners) {
+      listener()
+    }
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener)
+  }
+
+  return {
+    getState, dispatch, subscribe
+  }
+}
---
+++ src/store/taskReducer.js
@@ -0,0 +1,16 @@
+import * as actions from './actionTypes'
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    case actions.taskUpdated: {
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+      newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload }
+      return newArray
+    }
+    case actions.taskDeleted:
+      return state.filter(task => task.id !== action.payload.id)
+    default:
+      return state
+  }
+}
