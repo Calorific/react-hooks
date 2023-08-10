543578f32363d8a8cf55102c69768e8e7834c767
Sergey Skorykh <kknd3390@mail.ru>
2023-08-10 19:36:22 +0700

    router v5 app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Redirect, Route, Switch, useLocation, useParams } from 'react-router-dom'
 
-function App() {
+const AppLayout = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+        <h1>App Layout</h1>
+        <NavLink to="/users" exact>Users List Page</NavLink>
+      </>
+  )
 }
 
-export default App;
+const UserProfile = () => {
+  const { userId } = useParams()
+  return (
+    <>
+      <h2>User Page</h2>
+      <h3>UserId: {userId}</h3>
+
+      <ul>
+        <li><NavLink to="/users" exact>Users List Page</NavLink></li>
+        <li><NavLink to={'/users/' + userId + '/edit'} exact>Edit this user</NavLink></li>
+      </ul>
+    </>
+  )
+}
+
+const UserEdit = () => {
+  const { userId } = useParams()
+  return (
+      <>
+        <h2>User Page</h2>
+        <h3>UserId: {userId}</h3>
+
+        <ul>
+          <li><NavLink to={'/users/' + userId + '/profile'} exact>User Profile Page</NavLink></li>
+          <li><NavLink to={'/users/' + ((+userId || 0) + 1) + '/profile'} exact>Another User</NavLink></li>
+          <li><NavLink to="/users" exact>Users List Page</NavLink></li>
+        </ul>
+
+      </>
+  )
+}
+
+const UsersList = () => {
+  return (
+    <>
+      <ul>
+        {[1, 2, 3, 4, 5].map(u => <li key={u}><NavLink to={`/users/${u}/profile`} exact>User {u}</NavLink></li>)}
+      </ul>
+    </>
+  )
+}
+
+const UsersLayout = () => {
+  const { pathname } = useLocation()
+  const { userId } = useParams()
+  return (
+      <>
+        <h1>Users Layout</h1>
+        <NavLink to="/" exact>Main Page</NavLink>
+
+        <br />
+
+        <Switch>
+          <Route path="/users" component={UsersList} exact />
+          <Route path="/users/:userId" render={() => <Redirect to={pathname + '/profile'} />} exact />
+          <Route path="/users/:userId/profile" component={UserProfile} exact />
+          <Route path="/users/:userId/edit" component={UserEdit} exact />
+          <Route path="/users/:userId/*"  render={() => <Redirect to={'/users/' + userId + '/profile'} />} />
+        </Switch>
+
+      </>
+  )
+}
+
+
+const App = () => {
+  return (
+      <>
+        <Switch>
+          <Route path="/users/:userId?/:type?" component={UsersLayout} />
+          <Route path="/" component={AppLayout} exact />
+          <Route path="*" render={() => <Redirect to={AppLayout} />} />
+        </Switch>
+      </>
+  )
+}
+
+export default App
